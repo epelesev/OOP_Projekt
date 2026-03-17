9e4db0b2f0ce90c36c2420b98ff4a82bea0bc898
eliisabet <epelesev@gmail.com>
2026-03-17 12:47:05 +0200

    lisasin klassid

--- src/Baarmen.java
+++ src/Baarmen.java
@@ -1,3 +1,4 @@
 public class Baarmen {
     //võtab faililugejalt joogid, paneb listi; võtab tinderilt soovid, annab algoritmimootorile soovid edasi, saab mootorilt vastu 5 soovitust, tinderile edasi swipeimiseks
+
 }
