60772a314d6f47692f0728a6fd06855d93df7fe5
eliisabet <epelesev@gmail.com>
2026-03-17 11:43:59 +0200

    uuendasin

--- src/Midagi.java
+++ src/Midagi.java
@@ -2,5 +2,6 @@ public class Midagi {
     public static void main(String[] args) {
         System.out.println("alko");
         System.out.println("69");
+        System.out.println("6-7 dab");
     }
 }
