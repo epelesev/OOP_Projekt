6c51f6082c66d4dd73635821bfa4edcc0706dcaf
eliisabet <epelesev@gmail.com>
2026-03-17 14:34:31 +0200

    jook.java tehtud

--- src/Jook.java
+++ src/Jook.java
@@ -1,7 +1,19 @@
+import java.util.List;
+
 public class Jook {
-    /*
-    Kirjeldab üht jooki
-    - hoiab joogi andmeid
-    - ei sisalda loogikat
-     */
+    String nimi;
+    String maitse;
+    List<String> koostisosad;
+
+    public Jook(String nimi, String maitse, List<String> koostisosad) {
+        this.nimi = nimi;
+        this.maitse = maitse;
+        this.koostisosad = koostisosad;
+    }
+
+    @Override
+    public String toSring() {
+        if (koostisosad.isEmpty()) return nimi + " (" + maitse +")";
+        return nimi " (" + maitse +") - koostisosad: " + String.join(", " koostisosad);
+    }
 }
