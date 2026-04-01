51c031855a547c54d683532522b9b97d2c4d7acf
eliisabet <epelesev@gmail.com>
2026-04-01 10:33:31 +0300

    muudatused

--- src/SoovitusteSüsteem.java
+++ src/SoovitusteSüsteem.java
@@ -6,5 +6,21 @@ public class SoovitusteSüsteem {
         List<Jook> soovitused = new ArrayList<>();
 
         for (Jook jook : kõik) {
+            if (kasutaja.onHinnatud(jook)) continue;
+            int skoor = 0;
+
+            for (Jook meeldib : kasutaja.liked) {
+                if (jook.kirjeldus.equals(meeldib.kirjeldus)) skoor++;
+            }
+
+            for (Jook meeldib : kasutaja.superLiked) {
+                if (jook.kirjeldus.equals(meeldib.kirjeldus)) skoor +=2;
+            }
+
+            if (skoor > 0) {
+                soovitused.add(jook);
+            }
+        }
+        return soovitused;
     }
 }
