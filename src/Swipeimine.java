e56a17c0ab6739097c3549d98083465c29e836a3
kartkiiver <kart.kiiver@gmail.com>
2026-04-01 10:27:13 +0300

    tegin asja

--- src/Swipeimine.java
+++ src/Swipeimine.java
@@ -2,6 +2,18 @@ public class Swipeimine {
     /*
     tegeleb swipe'i loogikaga
     - lisab joogi õigesse listi (liked/dislike/super-liked)
-    - tagab, et sama jook ei oleks mitmes listis korraga (ei saa olla lika ja dislike)
+    - tagab, et sama jook ei oleks mitmes listis korraga (ei saa olla like ja dislike)
      */
+
+    public static void swipe(Kasutaja kasutaja, Jook jook, String tegevus) {
+        kasutaja.eemaldaKoikjalt(jook);
+
+        if (tegevus.equals("l")) {
+            kasutaja.liked.add(jook);
+        } else if (tegevus.equals("s")) {
+            kasutaja.superLiked.add(jook);
+        } else if (tegevus.equals("d")) {
+            kasutaja.disliked.add(jook);
+        }
+    }
 }
