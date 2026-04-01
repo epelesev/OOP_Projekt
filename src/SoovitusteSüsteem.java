a6bf0f02074b38c77b8f7735345d3e58fab00eb5
eliisabet <epelesev@gmail.com>
2026-04-01 10:27:47 +0300

    muudatused

--- src/SoovitusteSüsteem.java
+++ src/SoovitusteSüsteem.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class SoovitusteSüsteem {
     public static List<Jook> soovita(Kasutaja kasutaja, List<Jook> kõik) {
+        List<Jook> soovitused = new ArrayList<>();
 
+        for (Jook jook : kõik) {
     }
 }
